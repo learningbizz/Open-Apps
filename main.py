cad7e4043b1399f4b61be49a0066e1c03990d9e5
LearningBizz <learningbizz@gmail.com>
2021-02-06 19:22:03 +0100

    Added base code
    The base of the code I created in the beginning. I will branch from here on

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#To enable the shebang, you have to give the file permission of execution: chmod +x name_of_the_file.py
+
+import sys
+import subprocess
+from program_preset import programming_preset
+
+def main():
+  args = sys.argv[1:]
+  
+  if not args:
+    print('Add an existing preset')
+    sys.exit(1)
+  
+  if args[0] == "programming":
+    programming_preset()
+
+if __name__ == '__main__':
+  main()
